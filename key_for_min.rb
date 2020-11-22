8bc8b9468b0576c315f06e333b0bce5f338ca71f
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-22 04:20:14 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,16 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash_name)
-
   min_key = nil
   min_value = nil
-
 hash_name.each do |key, value|
   if min_value == nil
     min_key = key
     min_value = value
-end
-
 end
 end
 
