c773419443d3d41d8031bef330b0579aa8732898
victoriaehrbar <victoriaehrbar@gmail.com>
2020-11-22 04:44:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,9 @@ def key_for_min_value(hash_name)
   min_key = nil
   min_value = nil
 hash_name.each do |key, value|
-  if min_value == nil
+  if min_value == nil || value < min_value
     min_key = key
     min_value = value
-  else
-    value < min_value
 end
 end
 
